15c3176bdd5e8371b683125febe2786055afc82d
Edmir Lôbo <ejuniorls@outlook.com>
2023-12-22 16:22:44 -0300

    aula 06 - 06 - Componente Editar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
 import { ListarPensamentoComponent } from './componentes/pensamentos/listar-pensamento/listar-pensamento.component';
 import { ExcluirPensamentoComponent } from './componentes/pensamentos/excluir-pensamento/excluir-pensamento.component';
+import { EditarPensamentoComponent } from './componentes/pensamentos/editar-pensamento/editar-pensamento.component';
 
 const routes: Routes = [
   {
     path: '',
     redirectTo: 'listarPensamento',
-    pathMatch: 'full'
+    pathMatch: 'full',
   },
   {
     path: 'criarPensamento',
@@ -20,7 +21,11 @@ const routes: Routes = [
   },
   {
     path: 'pensamentos/excluirPensamento/:id',
-    component: ExcluirPensamentoComponent
+    component: ExcluirPensamentoComponent,
+  },
+  {
+    path: 'pensamentos/editarPensamento/:id',
+    component: EditarPensamentoComponent,
   },
 ];
 
